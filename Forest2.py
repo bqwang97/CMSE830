6f5fab374d8acde9c42f659d708c11f9ae0ebadd
bqwang97 <145512632+bqwang97@users.noreply.github.com>
2023-12-04 21:30:36 -0500

    Update Forest2.py

--- Forest2.py
+++ Forest2.py
@@ -101,10 +101,10 @@ with tab2:
     col1,col2=st.columns(2,gap='small')
 
     pairplot_options_x = col1.multiselect('Select features for x-axis of pairplot:',df_forest.drop(columns=['month','day','X', 'Y']).columns,default = "temp")
-    pairplot_options_y = col1.multiselect('Select features for y-axis of pairplot:',df_forest.drop(columns=['month','day','X', 'Y']).columns,default = "Logarea")
-    #pairplot_hue = st.sidebar.select_slider('Select hue for matrixplot:',options=['None','month', 'day'],value='None')
-    #hue = pairplot_hue if pairplot_hue != 'None' else None
-    fig1 = sns.pairplot(data=df_forest,x_vars=pairplot_options_x,y_vars=pairplot_options_y, kind='scatter')
+    pairplot_options_y = col2.multiselect('Select features for y-axis of pairplot:',df_forest.drop(columns=['month','day','X', 'Y']).columns,default = "Logarea")
+    pairplot_hue = st.sidebar.select_slider('Select hue for matrixplot:',options=['None','month', 'day'],value='None')
+    hue = pairplot_hue if pairplot_hue != 'None' else None
+    fig1 = sns.pairplot(data=df_forest,x_vars=pairplot_options_x,y_vars=pairplot_options_y, hue=pairplot_hue)
     st.pyplot(fig1)
  ####################################################################################################################################################################
     tab8, tab9,tab10 = st.tabs(["Heatmap", "Jointplot","Histogram"])
